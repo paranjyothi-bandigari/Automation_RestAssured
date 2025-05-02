c2ee50253b8fde0761a43d64fe5a9fd253eab87c
paranjyothi-bandigari <paranjyothi1994@gmail.com>
2025-05-01 20:23:48 -0600

    new

--- src/test/java/com/thetestingacadamy/POST/NonBDD_Post.java
+++ src/test/java/com/thetestingacadamy/POST/NonBDD_Post.java
@@ -2,16 +2,17 @@ package com.thetestingacadamy.POST;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
 import org.testng.annotations.Test;
 
-public class NonBDD_Post {
+public class NonBDD_Post{
     @Test
     public void get_token(){
      RequestSpecification r= RestAssured.given();
-
+//String token;
         String payload="{\n" +
                 "    \"username\" : \"admin\",\n" +
                 "    \"password\" : \"password123\"\n" +
@@ -23,6 +24,11 @@ public class NonBDD_Post {
           Response response =r.when().post();
         ValidatableResponse vr=response.then();
                vr.log().all().statusCode(200);
-
+  //token = response.jsonPath().getString("token");
+        //System.out.println(token);
+       // return token;
+        JsonPath jsonPath=new JsonPath(response.asString());
+        String token=jsonPath.getString("token");
+        System.out.println(token);
     }
 }
